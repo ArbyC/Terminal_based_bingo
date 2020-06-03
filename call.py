af1d3346b166b8b051a2cb198d01ea4043767fad
ArbitraryConstant <sandesh.sharma.sandesh@gmail.com>
2020-06-03 07:25:53 +0545

    refactored

--- call.py
+++ call.py
@@ -2,31 +2,35 @@ import random
 
 
 calling = list(range(1, 26))
+
+
 def computer_caller(calling):
     if len(calling) != 0:
         random.shuffle(calling)
         return calling.pop()
-def user_caller(calling):    
-    user_call = input('Enter your call: ')
-    if str(user_call) == "Q":
+
+
+def player_caller(calling):    
+    player_call = input('Enter your call: ')
+    if str(player_call) == "Q":
         return "Exit"
     while True:
         try:
-            if str(user_call) == "Q":
+            if str(player_call) == "Q":
                 return "Exit"
             else:
                 while True:
-                    if int(user_call) not in calling:
+                    if int(player_call) not in calling:
                         print("Already used. Try another number")
-                        user_call = input('Enter your call: ')
-                        if str(user_call) == "Q":
+                        player_call = input('Enter your call: ')
+                        if str(player_call) == "Q":
                             return "Exit"
                     else:
-                        return calling.remove(int(user_call))
+                        return calling.remove(int(player_call))
         except ValueError:
             print("Not a number")
-            user_call = input('Enter your call: ')
-            if str(user_call) == "Q":
+            player_call = input('Enter your call: ')
+            if str(player_call) == "Q":
                 return "Exit"
         else:
             break
